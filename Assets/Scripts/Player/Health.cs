c5a27d362450b403d3d62ce38528b3f624d937a6
Amanda Dzaferovic <s200100@students.aie.edu.au>
2021-12-09 21:09:39 +1030

    Removed health bar + collision type
    Set player collision type to continuous so it doesn't get stuck on walls

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    [Tooltip("Used as a temporary death animation")]
+    [SerializeField] private ConfigurableJoint hipJoint;
+
+    [ContextMenu("Kill player")]
+    private void KillPlayer()
+    {
+        TakeDamage(100.0f);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        // Subtract the damage
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Debug.Log("Player is dead");
+
+
+
+            JointDrive jointDrive = hipJoint.angularXDrive;
+            jointDrive.positionSpring = 0f;
+            hipJoint.angularXDrive = jointDrive;
+
+
+            JointDrive jointDriveYZ = hipJoint.angularYZDrive;
+            jointDriveYZ.positionSpring = 0f;
+            hipJoint.angularYZDrive = jointDriveYZ;
+        }
+    }
+
+}
