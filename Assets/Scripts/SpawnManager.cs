6ecece6f2d594a13f322416305bb4614e0c3f4b4
Felix <s200106@students.aie.edu.au>
2021-12-09 11:26:10 +1030

    Managers setup + timer

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] spawnPositions = new Transform[4];
+
+    /*public void SpawnCharacters(Character[] characters)
+    {
+        for (var i = 0; i < characters; i++)
+        {
+            characters[i].Spawn[spawnPositions[i]];
+        }
+    }*/
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hi");
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    #region Singleton
+    
+    public static UIManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(Instance);
+        }
+
+        Instance = this;
+    }
+    
+    #endregion
+
+    [Header("Canvases")] 
+    public Canvas pauseCanvas;
+    public Canvas winCanvas;
+    
+    public TextMeshProUGUI[] playerScores = new TextMeshProUGUI[4];
+    
+    [Space]
+    
+    public TextMeshProUGUI timerText;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        TurnPlayerScoresOn(false);
+    }
+
+    private void TurnPlayerScoresOn(bool turnOn = true)
+    {
+        foreach (var scoreText in playerScores)
+        {
+            scoreText.gameObject.SetActive(turnOn);
+        }
+    }
+
+    public void ShowPauseMenu(bool showPauseMenu = true) => pauseCanvas.gameObject.SetActive(showPauseMenu);
+    
+    public void ShowWinCanvas(bool showWinCanvas = true) => winCanvas.gameObject.SetActive(showWinCanvas);
+
+}
