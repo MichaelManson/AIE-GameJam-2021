add25acf134c493fbedace26f4441b89a1cd4988
MichaelManson <84944327+MichaelManson@users.noreply.github.com>
2021-12-09 16:26:23 +1030

    gingerbread person

---
+++ Assets/Scripts/RoofTiles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofTiles : MonoBehaviour
+{
+    Rigidbody rb;
+    public bool steppedOn;
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+}
