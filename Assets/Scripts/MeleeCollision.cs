c32e91414b096e5195fadc78664a1ee7d4768a7b
Amanda Dzaferovic <s200100@students.aie.edu.au>
2021-12-09 19:17:07 +1030

    Added melee + knockback
    Added a quick coroutine which enables the box (this will be removed and replaced with animation event)

--- Assets/Scripts/MeleeCollision.cs
+++ Assets/Scripts/MeleeCollision.cs
@@ -1,19 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MeleeCollision : MonoBehaviour
 {
+    [SerializeField] private float knockbackForce = 2000.0f;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.parent.TryGetComponent<PlayerController>(out PlayerController something))
+        // Attempt to get the rigidbody from the object of collision
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+
+        if(rb != null)
         {
-            if (something == null)
+            rb.AddForce(transform.forward * knockbackForce);
+        }
+        // Otherwise access the playerController of the enemy and apply force
+        else if(other.transform.parent.TryGetComponent<PlayerController>(out PlayerController enemy))
+        {
+            if (enemy == null)
                 return;
+
             Debug.Log("Knockback");
 
-            something.Hips.AddForce(Vector3.right * 3000.0f);
+            enemy.Hips.AddForce(transform.forward * knockbackForce);
         }
 
     }
