b71a8b1986c3075afba33a8467078014625d5091
Amanda Dzaferovic <s200100@students.aie.edu.au>
2021-12-09 23:08:45 +1030

    Started implementing lobby scene
    - Players can join on start button
    - Manager checks if all is ready

---
+++ Assets/Scripts/PlayerConfigManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerConfigManager : MonoBehaviour
+{
+    public static PlayerConfigManager Instance;
+
+    [SerializeField] private GameObject playerProfilePrefab;
+
+    public Transform[] points;
+
+    private List<PlayerConfig> playersJoined = new List<PlayerConfig>();
+
+    private int count = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SpawnPlayerProfile(PlayerInput input)
+    {
+        //GameObject player = Instantiate(playerProfilePrefab);
+        input.transform.parent = points[count];
+        input.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+
+        count++;
+
+        // Keep track of currently-joined players
+        playersJoined.Add(input.GetComponent<PlayerConfig>());
+    }
+
+
+    /// <summary>
+    /// Sent by PlayerConfig, the manager
+    /// checks if all players have readied up
+    /// </summary>
+    public void OnReady()
+    {
+        if (playersJoined.Count == 0)
+            return;
+
+        for(int i = 0; i < playersJoined.Count; i++)
+        {
+            // If a player isn't ready, exit
+            if (!playersJoined[i].IsReady)
+                return;
+        }
+
+        Debug.Log("All players ready. Proceed!");
+    }
+}
