020883ef07d96f9b6e28971d514237d238bb7ee3
MichaelManson <84944327+MichaelManson@users.noreply.github.com>
2021-12-09 16:30:10 +1030

    fixed roof tile script

--- Assets/Scripts/RoofTiles.cs
+++ Assets/Scripts/RoofTiles.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class RoofTiles : MonoBehaviour
 {
     Rigidbody rb;
-    public bool steppedOn;
+    BoxCollider _collider;
+    public bool steppedOn = false;
 
     private void OnEnable()
     {
         rb = GetComponent<Rigidbody>();
+        _collider = GetComponent<BoxCollider>();
+    }
+
+    //just for testing
+    private void Update()
+    {
+        if (steppedOn == true)
+        {
+            DropTile();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-
+            DropTile();
         }
+    }
+
+    IEnumerator DropTile()
+    {
+        yield return new WaitForSeconds(1f);
+        rb.useGravity = true;
+        _collider.enabled = false;
     }
 }
