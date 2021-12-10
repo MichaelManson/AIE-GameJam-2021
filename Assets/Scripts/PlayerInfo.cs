ee1add7074e231d677cef649d993913cb1579856
Amanda Dzaferovic <s200100@students.aie.edu.au>
2021-12-10 15:18:25 +1030

    Added player info on top of player's head
    Added extras
    - Timer countdown begins

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playerText;
+    [SerializeField] private TextMeshProUGUI readyText;
+
+    private PlayerConfig trackedPlayer;
+
+    private Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readyText.enabled = false;
+
+        mainCam = Camera.main;
+    }
+
+    public void Initialise(PlayerConfig player)
+    {
+        playerText.text = "P" + player.PlayerId;
+        trackedPlayer = player;
+    }
+
+    private void Update()
+    {
+        // Ensures the ui is always looking directly at the camera
+        transform.forward = mainCam.transform.forward;
+    }
+
+    public void PlayerConfig_OnReadyEvent()
+    {
+        Debug.Log("Calling ready ui");
+        readyText.enabled = !readyText.enabled;
+    }
+}
