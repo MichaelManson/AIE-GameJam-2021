a9a3f150ac73287a5707a495a78998e59a134bd3
MichaelManson <84944327+MichaelManson@users.noreply.github.com>
2021-12-10 16:47:03 +1030

    milk rises

---
+++ Assets/Scripts/MilkRise.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkRise : MonoBehaviour
+{
+    public bool milkRises;
+    float yPos = -15f;
+    public float yPosSpeed = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (milkRises)
+        {
+            yPos += yPosSpeed * Time.deltaTime;
+            this.transform.position = new Vector3(0, yPos, 0);
+        }
+    }
+}
